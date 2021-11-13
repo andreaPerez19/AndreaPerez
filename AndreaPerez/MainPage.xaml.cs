8aa73405c303151b35cf26a2b63f49c41d4b0266
andreaPerez19 <andre.g0397@gmail.com>
2021-11-12 21:45:41 -0500

    Agregar archivos de proyecto.

---
+++ AndreaPerez/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AndreaPerez
+{
+    public partial class MainPage : ContentPage
+    {
+        private const string Url = "http://192.168.100.245/moviles/post.php";
+        private readonly HttpClient client = new HttpClient();
+        private ObservableCollection<AndreaPerez.Datos> _post;
+        public MainPage()
+        {
+            InitializeComponent();
+            get();
+        }
+
+        private async void get()
+        {
+            var content = await client.GetStringAsync(Url);
+            List<AndreaPerez.Datos> post = JsonConvert.DeserializeObject<List<AndreaPerez.Datos>>(content);
+            _post = new ObservableCollection<AndreaPerez.Datos>(post);
+
+            MyListView.ItemsSource = _post;
+
+        }
+
+        private async void btnGet_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new viweInsertarB());
+
+        }
+
+        private async void btnPost_Clicked(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnDelete_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ AndreaPerez/viweInsertarB.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AndreaPerez
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class viweInsertarB : ContentPage
+    {
+        public viweInsertarB()
+        {
+            InitializeComponent();
+        }
+
+        private async void btningresar_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                WebClient cliente = new WebClient();
+                var parametros = new System.Collections.Specialized.NameValueCollection();
+                parametros.Add("codigo", txtCodigo.Text);
+                parametros.Add("nombre", txtNombre.Text);
+                parametros.Add("apellido", txtApellido.Text);
+                parametros.Add("edad", txtEdad.Text);
+
+                cliente.UploadValues("http://192.168.100.245/moviles/post.php", "POST", parametros);
+
+                await DisplayAlert("alerta","ingreso correcto","ok");
+            }
+            catch(Exception ex)
+            {
+                await DisplayAlert("alerta", ex.Message, "ok");
+            }
+        }
+
+        private async void btnregresar_Clicked(object sender, EventArgs e)
+        {
+           await Navigation.PushAsync(new MainPage());
+        }
+    }
+}
